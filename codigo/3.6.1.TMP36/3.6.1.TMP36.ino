f5de82769c49b25ffba4ebd75e397120e7158a47
javacasm <javacasm@gmail.com>
2021-11-17 13:25:10 +0100

    update

--- codigo/3.6.1.TMP36/3.6.1.TMP36.ino
+++ codigo/3.6.1.TMP36/3.6.1.TMP36.ino
@@ -1,20 +1,14 @@
-const int tempPin = 4;     //analog input pin constant<br>
-int tempVal;    // temperature sensor raw readings
-float volts;    // variable for storing voltage 
-float temp;     // actual temperature variable
-void setup()
-{
-  // start the serial port at 9600 baud
+const int tempPin = 4;     //Pin del sensor TMP36
+
+void setup(){
   Serial.begin(115200);
 }
-void loop()
-{
- //read the temp sensor and store it in tempVal
- tempVal = analogRead(tempPin);
- volts = tempVal/1023.0;             // normalize by the maximum temperature raw reading range
- temp = volts*33-5;
-Serial.print(" Temperature is:   "); // print out the following string to the serial monitor
-Serial.print(temp);                  // in the same line print the temperature
-Serial.println (" degrees C");       // still in the same line print degrees C, then go to next line.
-delay(1000);                         // wait for 1 second or 1000 milliseconds before taking the next reading. 
+void loop(){
+  int tempVal = analogRead(tempPin);
+  float volts = tempVal/1023.0;        // normalizamos a 0-1.0
+  float temp = volts*33-5;             // aplicamos la formula del fabricante
+  Serial.print("temperatura: "); 
+  Serial.print(temp);                  
+  Serial.println (" grados C");       
+  delay(1000);                         
 }
