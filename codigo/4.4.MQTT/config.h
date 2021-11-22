f3708b11ce6a188979e30e63f27fc17cb10a5b1c
javacasm <javacasm@gmail.com>
2021-11-22 13:11:32 +0100

    update

---
+++ codigo/4.4.MQTT/config.h
@@ -0,0 +1,6 @@
+const char* ssid = "jazzBajo";
+const char* password = "qazxcvbgtrewsdf";
+
+const char* MQTT_BROKER_ADRESS = "192.168.1.150";
+const uint16_t MQTT_PORT = 1883;
+const char* MQTT_CLIENT_NAME = "ESP32Client_1";
